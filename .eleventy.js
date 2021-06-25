9c7b0a5efd2fe4a91f3aedecc1ef3b28bb4ae488
Andy Bell <me@andy-bell.co.uk>
2021-06-25 22:29:10 +0100

    Base design tokens etc

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = config => {
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
 
+  config.addPassthroughCopy('./src/fonts');
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
